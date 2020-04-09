5473024629a18a4365138ecb18c3c469257ba440
Jesse Langley <jlangleyiii@aol.com>
2020-04-09 02:07:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-#require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -6,6 +6,7 @@ end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
+  binding.pry
 end
 
 def engage
